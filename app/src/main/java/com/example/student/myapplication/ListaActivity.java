7735adb1a3861bba1d8970c3bc51e26b489ddc5c
unknown <student@IBLAB19N-05.ib.polsl.pl>
2017-12-11 16:48:08 +0100

    Niepelne przejscie na nowe activity

--- app/src/main/java/com/example/student/myapplication/ListaActivity.java
+++ app/src/main/java/com/example/student/myapplication/ListaActivity.java
@@ -2,6 +2,7 @@ package com.example.student.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ListaActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class ListaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/student/myapplication/MainActivity.java
+++ app/src/main/java/com/example/student/myapplication/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.student.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button chnageBtn = findViewById(R.id.zmien);
+        final EditText chnageEdt = findViewById(R.id.wpisz);
+        final TextView text = findViewById(R.id.miasto);
+        Button chnageBtnn = findViewById(R.id.pokaz);
+
+
+        chnageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText(chnageEdt.getText().toString());
+            }
+        });
+
+        chnageBtnn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toy = new Intent(MainActivity.this,ListaActivity.Class);
+            }
+        });
     }
 }
